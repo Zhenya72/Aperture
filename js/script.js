cec47615dbc34c5783799b00cf2d67007d6349c6
Zhenya <yevheniy.bar@gmail.com>
2023-10-06 21:10:17 +0300

    finish verstka

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// window.addEventListener('scroll', function() {
+//     var element = document.getElementById('animation_box');
+//     var elementPosition = element.getBoundingClientRect().top;
+  
+//     if (elementPosition < window.innerHeight) {
+//       element.classList.add('sc_1_box');
+//     }
+//   });
+  
