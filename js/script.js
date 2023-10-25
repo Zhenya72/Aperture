d592cd23d5f81416489569ed18e27076787a2ee5
Zhenya <yevheniy.bar@gmail.com>
2023-10-25 22:10:56 +0300

    adaptiv to section4

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
-// window.addEventListener('scroll', function() {
-//     var element = document.getElementById('animation_box');
-//     var elementPosition = element.getBoundingClientRect().top;
-  
-//     if (elementPosition < window.innerHeight) {
-//       element.classList.add('sc_1_box');
-//     }
-//   });
-  
+const toggleButton = document.querySelector('.burger');
+const menu = document.querySelector('.header_right');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('#body');
+const menuLinks = document.querySelectorAll('.header_nav_list a');
+
+
+toggleButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    toggleButton.classList.toggle('active');
+    menu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    body.classList.toggle('active');
+});
+
+overlay.addEventListener('click', function(event) {
+    if (event.target === this) {
+      toggleButton.classList.remove('active');
+      menu.classList.remove('active');
+      overlay.classList.remove('active');
+      body.classList.remove('active');
+    }
+  });
+
+
+menuLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    toggleButton.classList.remove('active');
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+    body.classList.remove('active');
+    let target = link.getAttribute('href');
+    window.location.href = target;
+  });
+});
