adac3f15f6ff51c2a8813f80e88f63267e932e55
Zhenya <yevheniy.bar@gmail.com>
2023-10-26 18:18:32 +0300

    add window  read more

--- js/script.js
+++ js/script.js
@@ -19,6 +19,10 @@ overlay.addEventListener('click', function(event) {
       menu.classList.remove('active');
       overlay.classList.remove('active');
       body.classList.remove('active');
+      document.getElementById("more-box1").style.display = "none";
+      document.getElementById("more-box2").style.display = "none";
+      document.getElementById("more-box3").style.display = "none";
+      document.getElementById("more-box4").style.display = "none";
     }
   });
 
@@ -32,4 +36,54 @@ menuLinks.forEach(function(link) {
     let target = link.getAttribute('href');
     window.location.href = target;
   });
+});
+
+
+
+
+document.getElementById("open-more-box1").addEventListener("click", function() {
+  document.getElementById("more-box1").style.display = "block";
+  overlay.classList.toggle('active');
+  body.classList.toggle('active');
+});
+document.getElementById("close-box1-content").addEventListener("click", function() {
+  document.getElementById("more-box1").style.display = "none";
+  overlay.classList.remove('active');
+  body.classList.remove('active');
+});
+
+
+document.getElementById("open-more-box2").addEventListener("click", function() {
+  document.getElementById("more-box2").style.display = "block";
+  overlay.classList.toggle('active');
+  body.classList.toggle('active');
+});
+document.getElementById("close-box2-content").addEventListener("click", function() {
+  document.getElementById("more-box2").style.display = "none";
+  overlay.classList.remove('active');
+  body.classList.remove('active');
+});
+
+
+document.getElementById("open-more-box3").addEventListener("click", function() {
+  document.getElementById("more-box3").style.display = "block";
+  overlay.classList.toggle('active');
+  body.classList.toggle('active');
+});
+document.getElementById("close-box3-content").addEventListener("click", function() {
+  document.getElementById("more-box3").style.display = "none";
+  overlay.classList.remove('active');
+  body.classList.remove('active');
+});
+
+
+document.getElementById("open-more-box4").addEventListener("click", function() {
+  document.getElementById("more-box4").style.display = "block";
+  overlay.classList.toggle('active');
+  body.classList.toggle('active');
+});
+document.getElementById("close-box4-content").addEventListener("click", function() {
+  document.getElementById("more-box4").style.display = "none";
+  overlay.classList.remove('active');
+  body.classList.remove('active');
 });
